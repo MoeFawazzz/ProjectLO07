e4ffba081261858e84820617ff0b05d1ec6583e6
Antoine dgme <antoine.danguillaume@utt.fr>
2025-06-18 17:04:17 +0200

    changement de nom et ajout d'un nouveau fichier

---
+++ app/view/responsable/formAjoutProjet.php
@@ -0,0 +1,7 @@
+<h2>Ajouter un projet</h2>
+
+<form method="post" action="index.php?controller=responsable&action=ajoutProjet">
+    <label for="label">Label du projet :</label>
+    <input type="text" id="label" name="label" required>
+    <input type="submit" value="Créer">
+</form>
